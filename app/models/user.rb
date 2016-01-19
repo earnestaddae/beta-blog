a520fac27e95fbd24a9cc4e4619f8871f689df0f
Ernest Addae <earnestaddae@gmail.com>
2016-01-19 15:56:14 +0000

    Associate user with articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  validates :user,
+  before_save {self.email = email.downcase}
+  has_many :articles, dependent: :destroy
+  validates :username,
             presence: true,
             uniqueness: {case_sensitive: false},
             length: {minimum: 4, maximum: 105}
