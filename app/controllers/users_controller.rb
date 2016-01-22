be997472f24cc6148c3b16a2b197e50bf1e8b132
Ernest Addae <earnestaddae@gmail.com>
2016-01-22 14:52:50 +0000

    Add edit and update actions and create form parial

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to Beta-Blog, #{@user.username}"
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Account successfully updated"
+      redirect_to articles_path
+    else
+      render :edit
+    end
+  end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+end
